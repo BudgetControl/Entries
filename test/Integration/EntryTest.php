665d5e9dd1d7e3d18dfe0c19631c7016e13b7829
Marco De Felice <marco.defelice@budgetcontrol.cloud>
2025-07-01 14:44:36 +0200

    update entry library

--- test/Integration/EntryTest.php
+++ test/Integration/EntryTest.php
@@ -4,13 +4,14 @@ namespace Budgetcontrol\Test\Integration;
 
 use MLAB\PHPITest\Entity\Json;
 use Budgetcontrol\Test\BaseCase;
+use Illuminate\Support\Facades\DB;
+use Budgetcontrol\Entry\Facade\Crypt;
 use Psr\Http\Message\ResponseInterface;
 use MLAB\PHPITest\Assertions\JsonAssert;
 use Psr\Http\Message\ServerRequestInterface;
 use Budgetcontrol\Entry\Controller\EntryController;
+use Budgetcontrol\Entry\Controller\ExpensesController;
 use Budgetcontrol\Entry\Controller\IncomingController;
-use Budgetcontrol\Entry\Facade\Crypt;
-use Illuminate\Support\Facades\DB;
 
 class EntryTest extends BaseCase
 {
@@ -81,5 +82,22 @@ class EntryTest extends BaseCase
         $decryptedNote = Crypt::decrypt($contentResult['note']);
         $this->assertEquals($payload['note'], $decryptedNote);
 
+    }
+
+    public function test_create_entry_data_with_no_note_data()
+    {
+        $payload = $this->makeRequest(-100);
+        $payload['note'] = null; // No note data
+        $argv = ['wsid' => 1];
+
+        $request = $this->createMock(ServerRequestInterface::class);
+        $request->method('getParsedBody')->willReturn($payload);
+        $response = $this->createMock(ResponseInterface::class);
+
+        $controller = new ExpensesController();
+        $result = $controller->create($request, $response, $argv);
+
+        $this->assertEquals(201, $result->getStatusCode());
+        
     }
 }
